3e8b9a8ffbadcef8337b3714a1179396b733f0d3
gugushabeso <besogugusha@yahoo.com>
2020-05-17 16:39:44 +0400

    Updates

---
+++ precache-manifest.7e232024e1556973684900fe723e13f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc0fc54e0acff629320565313836e34e",
+    "url": "/monsters--rolodex/index.html"
+  },
+  {
+    "revision": "2d75354572aaa24e021b",
+    "url": "/monsters--rolodex/static/css/main.db2ef89e.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters--rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters--rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d75354572aaa24e021b",
+    "url": "/monsters--rolodex/static/js/main.631e8f6f.chunk.js"
+  },
+  {
+    "revision": "ffa08de537b1fa87d807",
+    "url": "/monsters--rolodex/static/js/runtime-main.32842799.js"
+  }
+]);
